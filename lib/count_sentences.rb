7874eb559005062061da7e4869d1b63d064c3701
Rawllings Kiprop <rutojr01@gmail.com>
2023-02-09 19:46:08 +0300

    Completed assignment

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,21 +3,25 @@ require 'pry'
 class String
 
   def sentence?
-    self[-1] == "."
+    #self[-1] == "."
+    self.end_with?(".")
     
   end
 
   def question?
-    self[-1] == "?"
+    #self[-1] == "?"
+    self.end_with?("?")
 
   end
 
   def exclamation?
-    self[-1] == "!"
+   # self[-1] == "!"
+   self.end_with?("!")
 
   end
 
   def count_sentences
+    #self.split(/[.!?]+/).count
     self.split(/[.!?]+/).count
 
   end
